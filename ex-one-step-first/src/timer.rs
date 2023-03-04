0f33bedf015ae4270c48606335c3cbb931e4807e
muhammad-abdulloh <clevercoderr@gmail.com>
2023-03-04 23:14:47 +0500

    todo non finished code error message

--- ex-one-step-first/src/timer.rs
+++ ex-one-step-first/src/timer.rs
@@ -1,3 +1,9 @@
 fn main(){
-    println!("This is a timer In shaa Alloh")
+    println!("This is a timer In shaa Alloh");
+
+    // Error chiqarberadi ekan yaramas zo'r narskan
+    // code tugamaganda yoki funksiya ohiriga yetmaganda todo ishlatsak
+    // codeni tugatmaganligimiz haqida exeption tashaydikan
+    // error message
+    todo!("Code generation non finished");
 }

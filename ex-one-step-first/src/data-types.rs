77e00c1bedb6a7ecc113f0739e9ee10360958213
muhammad-abdulloh <clevercoderr@gmail.com>
2023-03-05 23:04:35 +0500

    vashshe pointer number types

--- ex-one-step-first/src/data-types.rs
+++ ex-one-step-first/src/data-types.rs
@@ -1,3 +1,11 @@
 fn main(){
-    println!("Test");
+    let _number_64 = 4.0;      // compiler infers the value to use the default type f64
+    let _number_32: f32 = 5.0; // type f32 specified via annotation
+
+    // Addition, Subtraction, and Multiplication
+    println!("1 + 2 = {} and 8 - 5 = {} and 15 * 3 = {}", 1u32 + 2, 8i32 - 5, 15 * 3);
+
+    // Integer and Floating point division
+    println!("9 / 2 = {} but 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
+
 }

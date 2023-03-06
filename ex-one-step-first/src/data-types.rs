c08e56b7fa8a70f242ac97b0094fc573e4ab065c
muhammad-abdulloh <clevercoderr@gmail.com>
2023-03-06 14:50:48 +0500

    Rust with char smileklarniyam poddeshka qilarkan

--- ex-one-step-first/src/data-types.rs
+++ ex-one-step-first/src/data-types.rs
@@ -1,4 +1,6 @@
 fn main(){
+
+    /*
     // let _number_64 = 4.0;      // compiler infers the value to use the default type f64
     // let _number_32: f32 = 5.0; // type f32 specified via annotation
 
@@ -9,5 +11,29 @@ fn main(){
     // Integer and Floating point division
     // Rust vashsheyakanueeee 
     println!("9 / 2 = {} but 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
+*/
+
+/* 
+    // boolean
+    let is_bigger = 1 > 2;
+    println!("Is 1 > 2 = {}", is_bigger);
+*/
+
+    // Char
+    let thechar_uppercase = 'D';
+    let thechar_lowercase = 'd';
+    let thechar_smile_face = '😄';
+
+    println!("D = {}", thechar_uppercase);
+    println!("d = {}", thechar_lowercase);
+    println!("smile = {}", thechar_smile_face);
+
+    /*
+    // output
+    D = D
+    d = d
+    smile = 😄  // smileni poddeshka qibtasho'radikan 
+ */
+
 
 }

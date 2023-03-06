e3e6084a6a3840e94c96c430075cfbb448282e80
muhammad-abdulloh <clevercoderr@gmail.com>
2023-03-06 14:31:03 +0500

    Rust Bro o'zgaruvchilarni typeni bervorish qanday go'zal

--- ex-one-step-first/src/data-types.rs
+++ ex-one-step-first/src/data-types.rs
@@ -1,11 +1,13 @@
 fn main(){
-    let _number_64 = 4.0;      // compiler infers the value to use the default type f64
-    let _number_32: f32 = 5.0; // type f32 specified via annotation
+    // let _number_64 = 4.0;      // compiler infers the value to use the default type f64
+    // let _number_32: f32 = 5.0; // type f32 specified via annotation
 
     // Addition, Subtraction, and Multiplication
+    // sonni opturib srazi type berib urvorsa bo'lardkan :)
     println!("1 + 2 = {} and 8 - 5 = {} and 15 * 3 = {}", 1u32 + 2, 8i32 - 5, 15 * 3);
 
     // Integer and Floating point division
+    // Rust vashsheyakanueeee 
     println!("9 / 2 = {} but 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
 
 }
